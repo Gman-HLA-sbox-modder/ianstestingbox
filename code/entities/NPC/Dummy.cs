df85408f535152be3c56223c7999616fad6655b6
OMGOMGplays <loke@jenjen.se>
2022-07-15 16:29:27 +0200

    Shit doesn't simulate!!!

--- code/entities/NPC/Dummy.cs
+++ code/entities/NPC/Dummy.cs
@@ -6,10 +6,44 @@ public partial class Dummy : NPC
 {
 	public override float SpawnHealth => 100;
 
+	private TimeSince TimeSinceLastIBAttempt;
+
+	public override void Spawn() 
+	{
+		base.Spawn();
+
+		SetModel("models/scientist/scientist.vmdl");
+
+		TimeSinceLastIBAttempt = 0;
+	}
+
 	[Event.Frame]
 	public void OnFrame()
 	{
 		DebugOverlay.Text( this.Health.ToString(), EyePosition );
+	}
+
+	[Event.Tick]
+	public override void Simulate(Client cl) 
+	{
+		base.Simulate(cl);
+
+		Log.Info(TimeSinceLastIBAttempt);
+
+		var randInt = Rand.Int(1, 1);
+
+		if (TimeSinceLastIBAttempt >= 5.0f)
+		{
+			if (randInt == 1) 
+			{
+				SetAnimParameter("b_idlebreaker", true);
+
+				TimeSinceLastIBAttempt = 0;
+			} else if (randInt != 1)
+			{
+				TimeSinceLastIBAttempt = 0;
+			}
+		}
 	}
 
 	public override void TakeDamage( DamageInfo info )
